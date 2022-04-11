f5cc05f9fd9bc69a59d51a7bf0f83dc12308524e
empty <emptemp0000@gmail.com>
2022-04-12 00:46:12 +0900

    add ps4_controller_test

---
+++ ps4_controller_test.py.py
@@ -0,0 +1,51 @@
+import os
+import pprint
+import pygame
+
+class PS4Controller(object):
+    controller = None
+    axis_data = None
+    button_data = None
+    hat_data = None
+
+    def __init__(self):
+        pygame.init()
+        pygame.joystick.init()
+        self.controller = pygame.joystick.Joystick(0)
+        self.controller.init()
+
+    def listen(self):
+        if not self.axis_data:
+            self.axis_data = {}
+
+        if not self.button_data:
+            self.button_data = {}
+            for i in range(self.controller.get_numbuttons()):
+                self.button_data[i] = False
+
+        if not self.hat_data:
+            self.hat_data = {}
+            for i in range(self.controller.get_numhats()):
+                self.hat_data[i] = (0, 0)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.JOYAXISMOTION:
+                    self.axis_data[event.axis] = round(event.value, 2)
+                elif event.type == pygame.JOYBUTTONDOWN:
+                    self.button_data[event.button] = True
+                elif event.type == pygame.JOYBUTTONUP:
+                    self.button_data[event.button] = False
+                elif event.type == pygame.JOYHATMOTION:
+                    self.hat_datapevent.hat = event.value
+
+
+                os.system('cls')
+                pprint.pprint(self.button_data)
+                pprint.pprint(self.axis_data)
+                pprint.pprint(self.hat_data)
+
+
+if __name__ == "__main__":
+    ps4 = PS4Controller()
+    ps4.listen()
